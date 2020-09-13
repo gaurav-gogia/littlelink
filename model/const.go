959567f8feadbf61af1719d0ea429519bcc8a870
Gaurav Gogia <desmondmiles36@gmail.com>
2020-09-13 23:32:18 +0530

    app constants

--- model/const.go
+++ model/const.go
@@ -7,9 +7,26 @@ const (
 
 // Constants for server routes
 const (
-	CREATELITTLE = "/createLittle"
-	GETBIG       = "/getBig"
+	CREATELITTLE = "/setSmall"
+	GETBIG       = "/getLong"
 )
 
 // DBSTRING constant is used to define location of database
 const DBSTRING = "./data"
+
+// BUCKETSTRING constant is used to define the bucket for k:v db
+const BUCKETSTRING = "linkdata"
+
+// LOGSTRING constant is used to define location of application logs
+const LOGSTRING = "./data/log"
+
+// LOGLINE is the constant used to define the long line for log files for formatting
+const LOGLINE = "------------------------------------------------------------"
+
+// Severity levels for log
+const (
+	INFO = iota
+	LOW
+	MEDIUM
+	HIGH
+)
